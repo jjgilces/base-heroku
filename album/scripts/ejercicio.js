9fa00c7f906c3d648724fe069181c842dda8731c
jjgilces <jjgilces@espol.edu.ec>
2022-06-20 10:23:32 -0500

    creacion del proyecto

---
+++ album/scripts/ejercicio.js
@@ -0,0 +1,11 @@
+// let ejecutarCodigo  =() =>{
+// alert("Hola, mundo!");
+// }
+// ejecutarCodigo()
+
+let title = document.getElementById("titulo1")
+title.textContent="Album de fotos";
+let arreglo = document.getElementsByClassName("text-muted")
+
+
+
